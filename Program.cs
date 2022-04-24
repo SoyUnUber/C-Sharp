c23a4ea39154bb30f6dcdc3715bc778558042a5c
Noname <102451761+SoyUnUber@users.noreply.github.com>
2022-04-24 10:30:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Primernumero;
+int Segundonumero;
+int suma;
+
+Console.WriteLine("Introduce el primer numero");
+Primernumero = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Introduce el segundo numero");
+Segundonumero = Convert.ToInt32(Console.ReadLine());
+suma = Primernumero + Segundonumero ;
+Console.WriteLine("El resultado de la suma es {0}",suma);
