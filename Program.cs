7d212543ee2a8cfd285fbee59a6f913305b27fe5
Noname <102451761+SoyUnUber@users.noreply.github.com>
2022-04-28 19:48:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Estructura alternativa de control SWITCH (Comportamiento similar a IF);
+char Letra;  //Caracter
+Console.WriteLine("Digite una letra");
+Letra = Convert.ToChar(Console.ReadLine()); //Char solamente nos lee UN caracter, NO PALABRAS, NO MAS DE UN CARACTER, PARA ESO USAMOS STRING
+switch(Letra)
+{
+    case 'a':
+        Console.WriteLine("A digitado la letra a");
+        break;
+    case 'e':
+        Console.WriteLine("A digitado la letra E");
+        break;
+    case 'i':
+        Console.WriteLine("A digitado la letra I");
+        break;
+    case 'o':
+        Console.WriteLine("A digitado la letra O");
+        break;
+    case 'u': 
+        Console.WriteLine("A digitado la letra U");
+        break;
+    default: Console.WriteLine("La letra digitada NO es una vocal");
+        break;
+}
+Console.ReadKey();
