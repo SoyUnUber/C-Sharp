afb7f1faa437e847627b193df2c1c22cce28f6b6
Noname <102451761+SoyUnUber@users.noreply.github.com>
2022-04-29 07:48:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Manejo del DO-WHILE 
+// Hace el ciclo Hasta que la condicion sea FALSA
+
+/* Estructura del DO
+ * {
+ *     Sentencia;
+ * } While(Condicion);
+ * 
+ */
+
+int clave, valida;
+clave = 123;
+do
+{
+    Console.WriteLine("Digite la clave: ");
+    valida = Convert.ToInt32(Console.ReadLine());
+    if (valida != clave)
+    {
+        
+        Console.WriteLine("CLAVE INVALIDA");
+    }
+} while (clave != valida);
+    Console.WriteLine("CLAVE VALIDA....!!!");
+    Console.ReadKey();
