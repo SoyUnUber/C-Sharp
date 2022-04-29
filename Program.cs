8a32883428bff498f4311f34b0a60c6a75641b67
Noname <102451761+SoyUnUber@users.noreply.github.com>
2022-04-29 09:41:10 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Uso del FOR (Bucle o Ciclos)
+
+/* Estructura del FOR
+ * 
+ * for (Inicializacion de variable; Condicion; Incremento)
+ * {
+ *     sentencia; (o cuerpo del for)
+ * }
+ * 
+ */
+//Haremos un CONTADOR
+int a;
+for (a=0; a<=50; a++) //Este contador va ir hasta que numero... osea 50. A sera menor de 50. A sumara 1 + 1  (a++) a = a + 1
+{
+    Console.WriteLine(a);
+}
+Console.ReadKey();
+
+/* Operador de Incremento
+a++;                   */
+/* Operador de Decremento
+b--;                    */
